1ed8874872463a4b0ff9a90871add6858da263a3
matthew-pate <mattpate@gmail.com>
2022-09-17 22:53:14 -0500

    added mobile hamburger menu to all pages

---
+++ style.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+    document.getElementById("mobile_menu_bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
